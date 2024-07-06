e5be8b9e3e6b35500b3c20e70992c3b46ed0c208
jamesramputh <jamesramputh@gmail.com>
2024-07-05 21:04:59 -0400

    Updated sendmail to old way

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,43 +1,37 @@
-import { ApiClient, TransactionalEmailsApi, SendSmtpEmail } from '@sendinblue/client';
-import dotenv from 'dotenv';
+import SibApiV3Sdk from 'sib-api-v3-sdk';
 
-dotenv.config();
-
-const defaultClient = ApiClient.instance;
-const apiKey = defaultClient.authentications['api-key'];
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+let apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
 
-const apiInstance = new TransactionalEmailsApi();
+var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-export default async (req, res) => {
-  if (req.method === 'POST') {
-    const { firstName, lastName, email, packageOption, message } = req.body;
+export default async function(req, res) {
+  const { firstName, lastName, email, packageOption, message } = req.body;
 
-    // Create text and HTML content for the email
-    let textContent = `${firstName} ${lastName} has inquired about a video. Package Option: ${packageOption}. Their email: ${email}. Message: ${message}.`;
-    let htmlContent = `<p>${firstName} ${lastName} has inquired about a video</p><p>Package Option: ${packageOption}</p><p>Their email: ${email}</p><p>Message: ${message}</p>`;
+  // Create text and HTML content for the email
+  let textContent = `${firstName} ${lastName} has inquired about a video. Package Option: ${packageOption}. Their email: ${email}. Message: ${message}.`;
+  let htmlContent = `<p>${firstName} ${lastName} has inquired about a video</p><p>Package Option: ${packageOption}</p><p>Their email: ${email}</p><p>Message: ${message}</p>`;
 
-    let sendSmtpEmail = new SendSmtpEmail();
-    sendSmtpEmail = {
-      to: [{
-        email: 'james.ramputh@gmail.com',
-      }],
-      sender: {
-        email: email,
-      },
-      subject: `Real estate video/photo Inquiry ${firstName} ${lastName}`,
-      htmlContent: htmlContent,
-      textContent: textContent
-    };
+  let sendSmtpEmail = new SendSmtpEmail();
+  sendSmtpEmail = {
+    to: [{
+      email: 'james.ramputh@gmail.com',
+    }],
+    sender: {
+      email: email,
+    },
+    subject: `Real estate video/photo Inquiry: ${firstName} ${lastName}`,
+    htmlContent: htmlContent,
+    textContent: textContent
+  };
 
-    try {
-      await apiInstance.sendTransacEmail(sendSmtpEmail);
-      res.status(200).send('Message sent successfully.');
-    } catch (error) {
-      console.error('ERROR', error);
-      res.status(400).send('Message not sent.');
-    }
-  } else {
-    res.status(405).send('Method Not Allowed');
+  try {
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    res.status(200).send('Message sent successfully.');
+  } catch (error) {
+    console.error('ERROR', error);
+    res.status(400).send('Message not sent.');
   }
-};
+
+}
